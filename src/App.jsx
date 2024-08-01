083990da7ac6baf80db4be19ce61104643b665c5
Nithinlinga <nithinlinga2@gamil.com>
2024-08-01 20:00:35 +0530

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,39 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [formData,setFormData] = useState({firstname:"",lastname:"",email:"",mobile:""})
+  function handleChange(event){
+    console.log(event.target.value);
+    setFormData(prevData=>{
+      return {
+        ...prevData,
+        [event.target.name]:event.target.value
+      }
+    })
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='bor'>
+        <form className='border-2 border-green-400'>
+          <input type="text" className='block' 
+          placeholder='Enter name'
+          name='firstname'
+          onAbort={handleChange}/>
+          <input type="text" className='block'
+          placeholder='Enter lastname'
+          name='lastname'
+          onAbort={handleChange}/>
+          <input type="text" className=''
+          placeholder='Enter email'
+          name='email'
+          onAbort={handleChange}/>
+          <input type="text" className='block' 
+          placeholder='Enter mobile'
+          name='mobile'
+          onAbort={handleChange}/>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className=' '></div>
     </>
   )
 }
